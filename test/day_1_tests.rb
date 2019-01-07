4f76d1f70b086e4e7bb694829ea3957daaf5cf0f
Mike Wilson <mike1234@gmail.com>
2019-01-07 13:54:29 -0800

    Formatting update.

--- test/day_1_tests.rb
+++ test/day_1_tests.rb
@@ -13,7 +13,7 @@ class Day1Tests < Minitest::Test
         assert_equal(10, Day1.problem_2([3, 3, 4, -2, -4]))
         assert_equal(5, Day1.problem_2([-6, 3, 8, 5, -6]))
         assert_equal(14, Day1.problem_2([7, 7, -2, -7, -4]))
-    end
+    end 
 
     def test_day_1_solution_1_final
         input = File.readlines("day_1/input.txt")
